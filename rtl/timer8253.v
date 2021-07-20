2303395629caf0363066a4535c3188fe4896cb48
Gyorgy Szombathelyi <gyurco@freemail.hu>
2021-07-20 20:28:54 +0200

    Some timer logic fixes
    - fix the latching mode
    - cs should not stop counting
    - reading the timer counts should not reload them

--- rtl/timer8253.v
+++ rtl/timer8253.v
@@ -45,16 +45,16 @@
 `timescale 1ns / 1ps
 
 module timer_8253(
-    input CS,
-	 input WR,
-    input [1:0]addr,
-	 input [7:0]din,
-	 output wire [7:0]dout,
-	 input CLK_25,
-	 input clk,		// cpu CLK
-	 output out0,
-	 output out2
-    );
+	input CS,
+	input WR,
+	input [1:0]addr,
+	input [7:0]din,
+	output wire [7:0]dout,
+	input CLK_25,
+	input clk,		// cpu CLK
+	output out0,
+	output out2
+	);
 	 
 	reg [8:0]rclk = 0; // rclk[8] oscillates at 1193181.8181... Hz
 	reg [1:0]cclk = 0;
@@ -77,67 +77,110 @@ module timer_8253(
 	wire [7:0]dout2;
 	wire [7:0]mode0;
 	wire [7:0]mode2;
-	
+
 	counter counter0 (
-		 .CS(CS && (a0 || cmd0)),
-		 .WR(WR), 
-		 .clk(clk), 
-		 .cmd(cmd0), 
-		 .din(din), 
-		 .dout(dout0), 
-		 .mode(mode0), 
-		 .CE(cclk)
-    );
+		.CS(CS && (a0 || cmd0)),
+		.WR(WR),
+		.clk(clk),
+		.cmd(cmd0),
+		.din(din),
+		.dout(dout0),
+		.out(out0),
+		.CE(cclk)
+	);
 
 	counter counter2 (
-		 .CS(CS && (a2 || cmd2)),
-		 .WR(WR), 
-		 .clk(clk), 
-		 .cmd(cmd2), 
-		 .din(din), 
-		 .dout(dout2), 
-		 .mode(mode2), 
-		 .CE(cclk)
-		 );
+		.CS(CS && (a2 || cmd2)),
+		.WR(WR),
+		.clk(clk),
+		.cmd(cmd2),
+		.din(din),
+		.dout(dout2),
+		.out(out2),
+		.CE(cclk)
+	);
 
-	assign out0 = mode0[7];
-	assign out2 = mode2[7];
 	assign dout = 	a0 ? dout0 : dout2;
 	
 endmodule
 
 
 module counter(
-	 input CS,
+	input CS,
     input WR,	// write cmd/data
-	 input clk,	// CPU clk
+	input clk,	// CPU clk
     input cmd,
-	 input [7:0]din,
-	 output [7:0]dout,
-	 output reg [7:0]mode,	// mode[7] = output
-	 input [1:0]CE	// count enable
-    );
+	input [7:0]din,
+	output [7:0]dout,
+	output out,
+	input [1:0]CE	// count enable
+	);
 	 
-	 reg [15:0]count = 0;
-	 reg [15:0]init = 0;
-	 reg [1:0]state = 0; // state[1] = init reg filled
-	 reg strobe = 0;
-	 reg rd = 0;
-	 reg latch = 0;
-	 reg newcmd = 0;
-	 wire c1 = count == 1;
-	 wire c2 = count == 2;
-	 reg CE1 = 0;
-	 
-	 assign dout = mode[5] & (~mode[4] | rd) ? count[15:8] : count[7:0];
-	 
+	reg [15:0]count = 0;
+	reg [15:0]init = 0;
+	reg [15:0]latched = 0;
+	reg [1:0]state = 0; // state[1] = init reg filled
+	reg strobe = 0;
+	reg rd = 0;
+	reg latch = 0;
+	reg newcmd = 0;
+	reg newdata = 0;
+	reg [6:0] mode; // mode[6] = output
+	wire c1 = count == 1;
+	wire c2 = count == 2;
+	reg CE1 = 0;
+
+	assign out = mode[6];
+	assign dout = latch ?
+		(rd ? latched[15:8] : latched[7:0]) : 
+		(mode[5] & (~mode[4] | rd) ? count[15:8] : count[7:0]);
+
 	always @(posedge clk) begin
 		
 		if(CE == 2'b10) CE1 <= 1;
 
+		if(CE1) begin
+			newcmd <= 0;
+			newdata <= 0;
+			CE1 <= 0;
+			case(mode[3:1])
+				3'b000, 3'b001:
+					if(newdata) begin
+						mode[6] <= 0;
+						count <= init;
+					end else begin
+						count <= count - 1'd1;
+						if(c1) mode[6] <= 1;
+					end
+				3'b010, 3'b110: begin
+					mode[6] <= ~c2;
+					if(c1 | newcmd) begin
+						count <= init;
+					end else count <= count - 1'd1;
+				end
+				3'b011, 3'b111: begin
+					if(c1 | c2 | newcmd) begin
+						mode[6] <= {~mode[6] | newcmd};
+						count <= {init[15:1], (~mode[6] | newcmd) & init[0]};
+					end else count <= count - 2'd2;
+				end
+				3'b100, 3'b101:
+					if(newdata | newcmd) begin
+						mode[6] <= 1;
+						count <= init;
+						strobe <= 1;
+					end else begin
+						count <= count - 1'd1;
+						if(c1) begin
+							if(strobe) mode[6] <= 0;
+							strobe <= 0;
+						end else mode[6] <= 1'd1;
+					end
+			endcase
+		end
+
 		if(CS) begin
 			if(WR) begin
-				mode[6] <= 1;
 				rd <= 0;
 				latch <= 0;
 				if(cmd) begin	// command
@@ -145,8 +188,12 @@ module counter(
 						mode[5:0] <= din[5:0];
 						newcmd <= 1;
 						state <= {1'b0, din[5] & ~din[4]};
-					end else latch <= &mode[5:4];
+					end else begin
+						latch <= 1;
+						latched <= count;
+					end
 				end else begin	// data
+					newdata <= 1;
 					state <= state[0] + ^mode[5:4] + 1'd1;
 					if(state[0]) init[15:8] <= din;
 					else init[7:0] <= din;
@@ -155,44 +202,6 @@ module counter(
 				rd <= ~rd;
 				if(rd) latch <= 0;
 			end
-		end else if(state[1] && CE1 && !latch) begin
-			newcmd <= 0;
-			CE1 <= 0;
-			case(mode[3:1])
-				3'b000, 3'b001:
-					if(mode[6]) begin
-						mode[7:6] <= 2'b00;
-						count <= init;
-					end else begin
-						count <= count - 1'd1;
-						if(c1) mode[7] <= 1;
-					end
-				3'b010, 3'b110: begin
-					mode[7] <= ~c2;
-					if(c1 | newcmd) begin
-						mode[6] <= 1'b0;
-						count <= init;
-					end else count <= count - 1'd1;
-				end
-				3'b011, 3'b111: begin
-					if(c1 | c2 | newcmd) begin
-						mode[7:6] <= {~mode[7] | newcmd, 1'b0};
-						count <= {init[15:1], (~mode[7] | newcmd) & init[0]};
-					end else count <= count - 2'd2;
-				end
-				3'b100, 3'b101:
-					if(mode[6]) begin
-						mode[7:6] <= 2'b10;
-						count <= init;
-						strobe <= 1;
-					end else begin
-						count <= count - 1'd1;
-						if(c1) begin
-							if(strobe) mode[7] <= 0;
-							strobe <= 0;
-						end else mode[7] <= 1'd1;
-					end
-			endcase
 		end
 	end
 
